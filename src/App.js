58ec5caa560c7903e5b68f149531658a341d29c6
profpython <profpython@github.com>
2022-06-08 22:58:40 -0230

    contact page update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} >
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
     </>
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,33 @@
+import './index.scss'
+import React, { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+
+const Portfolio = () => {
+    const [letterClass, setLetterClass] = useState('text-animate') 
+
+    useEffect(() => {
+        setTimeout(()=>{
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+    return(
+        <>
+            <div className='container portfolio-page'>
+                <h1 className='page-title'>
+                    <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={['P','o','r','t','f','o','l','i','o']}
+                    idx={15}
+                    />
+                </h1>
+                {/* <div>{renderPortfolio()}</div> */}
+            </div>
+            <Loader type = 'pacman'/>
+        </>
+    )
+}
+
+
+export default Portfolio
