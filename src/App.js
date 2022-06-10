630ece021b67ef6f8fa32164a4c44d4d1ef5d5a8
profpython <profpython@github.com>
2022-06-10 02:38:12 -0230

    update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />} >
-        <Route path="/about" element={<About />} />
+        <Route path="about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route index element={<Home />} />
